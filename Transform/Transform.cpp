0486ffa7e48865ec06d6ffa2ba8b25ed226b5e64
Dawn Yue <1179348863@qq.com>
2020-03-30 18:50:54 +0800

    课前准备

--- Transform/Transform.cpp
+++ Transform/Transform.cpp
@@ -1,20 +1,65 @@
-﻿// Transform.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿#include <opencv2/opencv.hpp>  
 #include <iostream>
+using namespace cv;
+using namespace std;
 
+#define WINDOW_NAME1 "【原始图窗口】"					//为窗口标题定义的宏 
+#define WINDOW_NAME2 "【经过Warp后的图像】"        //为窗口标题定义的宏 
+#define WINDOW_NAME3 "【经过Warp和Rotate后的图像】"        //为窗口标题定义的宏 
+//Transform
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+	//【1】参数准备
+	//定义两组点，代表两个三角形
+	Point2f srcTriangle[3];
+	Point2f dstTriangle[3];
+	//定义一些Mat变量
+	Mat rotMat(2, 3, CV_32FC1);
+	Mat warpMat(2, 3, CV_32FC1);
+	Mat srcImage, dstImage_warp, dstImage_warp_rotate;
+
+	//【2】加载源图像并作一些初始化
+	srcImage = imread("E:\\1.png");
+	if (!srcImage.data) { printf("读取图片错误，请确定目录下是否有imread函数指定的图片存在~！ \n"); return false; }
+	// 设置目标图像的大小和类型与源图像一致
+	dstImage_warp = Mat::zeros(srcImage.rows, srcImage.cols, srcImage.type());
+
+	//【3】设置源图像和目标图像上的三组点以计算仿射变换
+	srcTriangle[0] = Point2f(0, 0);
+	srcTriangle[1] = Point2f(static_cast<float>(srcImage.cols - 1), 0);
+	srcTriangle[2] = Point2f(0, static_cast<float>(srcImage.rows - 1));
+
+	dstTriangle[0] = Point2f(static_cast<float>(srcImage.cols*0.0), static_cast<float>(srcImage.rows*0.33));
+	dstTriangle[1] = Point2f(static_cast<float>(srcImage.cols*0.65), static_cast<float>(srcImage.rows*0.35));
+	dstTriangle[2] = Point2f(static_cast<float>(srcImage.cols*0.15), static_cast<float>(srcImage.rows*0.6));
+
+	//【4】求得仿射变换
+	warpMat = getAffineTransform(srcTriangle, dstTriangle);
+
+	//【5】对源图像应用刚刚求得的仿射变换
+	warpAffine(srcImage, dstImage_warp, warpMat, dstImage_warp.size());
+
+	//【6】对图像进行缩放后再旋转
+	// 计算绕图像中点顺时针旋转50度缩放因子为0.6的旋转矩阵
+	Point center = Point(dstImage_warp.cols / 2, dstImage_warp.rows / 2);
+	double angle = -50.0;
+	double scale = 0.6;
+	// 通过上面的旋转细节信息求得旋转矩阵
+	rotMat = getRotationMatrix2D(center, angle, scale);
+	// 旋转已缩放后的图像
+	warpAffine(dstImage_warp, dstImage_warp_rotate, rotMat, dstImage_warp.size());
+
+
+	//【7】显示结果
+	imshow(WINDOW_NAME1, srcImage);
+	imshow(WINDOW_NAME2, dstImage_warp);
+	imshow(WINDOW_NAME3, dstImage_warp_rotate);
+
+	// 等待用户按任意按键退出程序
+	waitKey(0);
+
+	return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
